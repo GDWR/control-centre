f6ded48373c16107c198543ec7a7c1f9def372d6
GDWR <gregory.dwr@gmail.com>
2024-05-11 12:58:38 +0100

    Add devshell

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A very basic flake";
+  outputs = { self, nixpkgs }: 
+    let 
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: function nixpkgs.legacyPackages.${system});
+    in {
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          opentofu
+	];
+      };
+    });
   };
 }
