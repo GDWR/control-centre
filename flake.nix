fa5f07c582d21e93d6ea03e8bf3ee7bffba82b49
GDWR <gregory.dwr@gmail.com>
2024-05-15 22:37:59 +0100

    Use default.nix

--- flake.nix
+++ flake.nix
@@ -6,21 +6,11 @@
     nixosConfigurations = {
       control = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./hosts/control/configuration.nix
-        ];
-      };
-      database = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	modules = [
-	  ./hosts/database/configuration.nix
-	];
+        modules = [ ./hosts/control ];
       };
       node = nixpkgs.lib.nixosSystem {
 	system = "x86_64-linux";
-	modules = [
-	  ./hosts/node/configuration.nix
-	];
+	modules = [ ./hosts/node ];
       };
     };
   };
